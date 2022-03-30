7e550fe3f54b8a84b03a344ccb8abc297e49527c
thetrueVenom <thetruevenom@gmx.de>
2022-03-30 07:46:00 +0200

    added User class

---
+++ User.java
@@ -0,0 +1,23 @@
+public class User {
+
+    String name;
+    int workingHours;
+
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setWorkingHours(int workingHours){
+        this.workingHours = workingHours;
+    }
+
+    public int getWorkingHours(){
+        return this.workingHours;
+    }
+
+}
